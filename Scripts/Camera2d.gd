d8c65f441af309548d4f93f6495e051c2888fb96
Ryan Badour <rybadour@gmail.com>
2022-09-24 00:05:42 -0400

    Added camera panning and resources list
     - Also reorganized files

---
+++ Scripts/Camera2d.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+class_name MainCamera
+
+var _previousPosition: Vector2 = Vector2(0, 0);
+var _moveCamera: bool = false;
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton && event.is_action("Drag"):
+		if event.is_pressed():
+			_previousPosition = event.position;
+			_moveCamera = true;
+		else:
+			_moveCamera = false;
+	elif event is InputEventMouseMotion && _moveCamera:
+		position += (_previousPosition - event.position);
+		_previousPosition = event.position;
