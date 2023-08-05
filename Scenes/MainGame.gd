fd7a2220fe50aa2ab961361aaf9fd941a9143549
rybadour <rybadour@gmail.com>
2023-08-05 15:41:10 -0400

    Worker will attack targetted rock

--- Scenes/MainGame.gd
+++ Scenes/MainGame.gd
@@ -5,13 +5,21 @@ extends Control
 
 var followTimer = Timer.new();
 var pathToFollow: PackedVector2Array;
+var attackTimer = Timer.new();
+var isWorkerMining: bool = false;
+var targetRock: RockUI;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	followTimer.connect("timeout", followTime);
 	followTimer.wait_time = 0.5;
 	add_child(followTimer);
-	tileMap.connect('cellClicked', targetRock);
+
+	attackTimer.connect("timeout", attackTime);
+	attackTimer.wait_time = 0.2;
+	add_child(attackTimer);
+
+	tileMap.connect('cellClicked', moveToAttackRock);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,15 +30,28 @@ func _process(delta):
 func followTime():
 	if pathToFollow.is_empty():
 		followTimer.stop();
+		isWorkerMining = true;
+		attackTimer.start();
 		return
 		
 	worker.position = pathToFollow[0];
 	pathToFollow.remove_at(0);
 
 
-func targetRock(cell: Vector2i):
+func attackTime():
+	if targetRock == null || targetRock.health <= 0:
+		isWorkerMining = false;
+		attackTimer.stop();
+		return;
+
+	targetRock.takeDamage(1);
+
+
+func moveToAttackRock(cell: Vector2i):
 	if !tileMap.rocks.has(cell):
 		return;
+
+	targetRock = tileMap.rocks[cell];
 
 	var fastestPath: PackedVector2Array;
 	for neighbour in tileMap.get_surrounding_cells(cell):
@@ -43,6 +64,7 @@ func targetRock(cell: Vector2i):
 
 
 func moveWorker(path: PackedVector2Array):
+	attackTimer.stop();
 	pathToFollow = path;
 	followTimer.start();
 
