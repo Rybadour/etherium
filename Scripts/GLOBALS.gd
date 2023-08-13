658792da3838493c8dfbdd83d7865a966a3b58c2
rybadour <rybadour@gmail.com>
2023-08-12 23:34:50 -0400

    Added ItemRarity enum

--- Scripts/GLOBALS.gd
+++ Scripts/GLOBALS.gd
@@ -3,13 +3,15 @@ extends Node
 enum AffixType {Prefix, Suffix}
 
 enum StatType {
-	MiningPower, IncreasedMiningPower, 
+	MiningPower, IncreasedMiningPower,
 	ActionSpeed, IncreasedActionSpeed,
 	FireDamage, IncreasedFireDamage,
-	OilCapacity, 
+	OilCapacity,
 	ChanceToFindItems,
 	QuantityOfOre,
 	MovementSpeed,
 }
 
 enum SlotType {Weapon, Head, Chest, Amulet, Ring, Gloves, Boots}
+
+enum ItemRarity {Common, Magic, Rare}
